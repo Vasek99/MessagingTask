d134b91285cece88fc8ee23f56f8f476e9c15ed0
Vasek99 <vasek1999@centrum.cz>
2021-07-08 18:41:40 +0200

    Add files via upload

---
+++ DeveloperTaskTomsicek/MessagingLibrary/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MessagingLibrary
+{
+    public class Messaging
+    {
+
+        public static string SendSMSMessage(string phoneNumberSender, string phoneNumberReciever, string text)
+        {
+            return "Message has been sent.";
+        }
+    }
+}
